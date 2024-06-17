6e259cdbe43a39a7a83104dd916ce265ebdcb641
unknown <aknegi97@gmail.com>
2024-06-17 13:59:02 +0530

    codeflow

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {  login, logout,  signup, userDetails } from '../controller/userController.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
+import { getMyCodes } from '../controller/compilerController.js';
 
 export const userRouter = express.Router();
 
@@ -10,3 +11,4 @@ userRouter.post("/logout", logout);
 
 userRouter.get("/my-details", verifyToken, userDetails);
 
+userRouter.get("/my-codes" , verifyToken, getMyCodes)
