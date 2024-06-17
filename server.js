d7f905e65b6104b3f8e64dd5c52c4913b512a0f5
unknown <aknegi97@gmail.com>
2024-06-17 13:39:10 +0530

    codeflow

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors ({
-//     origin : "http://localhost:5173", 
-//     credentials : true
-// }))
+app.use(cors ({
+    credentials: true,
+    origin: "http://localhost:5173"
+}))
 
 app.use(cors());
 
